e5edb94f66e6e63ed6ed42041630432d69b41265
Gabriel <mgabriel.moliveira@gmail.com>
2022-09-27 18:53:24 -0300

    adicionado função de multiplos items no carrinho

---
+++ shopping-cart/src/shoppingCartProject/body/shoppingList/MockItemsList/ItemsListmap/index.jsx
@@ -0,0 +1,78 @@
+import React, { useContext } from "react";
+import { Icons } from "../../../../constants";
+import { ExchangeCoinContext } from "../../../../exchangeCoinContext";
+
+export const ItemsListMapping = ({
+  colors,
+  image,
+  category,
+  name,
+  price,
+  quantity,
+  colorSelect,
+  sizes,
+  onEventHandler,
+  buyButtonHandler,
+}) => {
+  const currentCoin = useContext(ExchangeCoinContext);
+
+  return (
+    <div className="flex  justify-evenly max-w-[350px]">
+      <div className="rounded-xl font-black p-4 border-2 shadow-[0_15px_60px_15px_rgba(0,0,0,0.1)]">
+        <div className="flex relative flex-col">
+          <img
+            className="rounded-xl lg:max-w-[200px]"
+            src={image}
+            alt={category}
+          />
+          <div className="flex flex-col flex ">
+            <span className="text-center text-xl pb-2">{name}</span>
+            {currentCoin[0].value === "Dolar - $" ? (
+              <span>Price: {price}</span>
+            ) : (
+              <span>Price: {(price * 5.1).toFixed(2)}</span>
+            )}
+            <span>Stock: {quantity}</span>
+            <select
+              className=" text-black rounded-xl mb-2 mt-2"
+              onChange={(e) => onEventHandler(e.target.value)}
+              value={colorSelect}
+            >
+              {colors.map((option) => (
+                <option
+                  className="w-20"
+                  key={option}
+                  value={option}
+                  label={option}
+                />
+              ))}
+            </select>
+            <select className="rounded-xl">
+              {sizes.map((option) => (
+                <option key={option} value={option} label={option} />
+              ))}
+            </select>
+          </div>
+          <div className="relative pt-12 hover:opacity-80 ">
+            <div className="absolute bottom-0 right-2 flex border-solid border-2 border-sky-500  rounded-full p-1 float-right cursor-pointer">
+              <Icons.CartIcon
+                width="20px"
+                height="20px"
+                className=""
+                fill="#3FBCF4"
+              />
+              <input
+                type="button"
+                value="Buy"
+                className="pl-1 pr-1 text-black"
+                onClick={() =>
+                  buyButtonHandler(name, price, colors, sizes, category)
+                }
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- shopping-cart/src/shoppingCartProject/body/shoppingList/MockItemsList/index.jsx
+++ shopping-cart/src/shoppingCartProject/body/shoppingList/MockItemsList/index.jsx
@@ -1,12 +1,10 @@
 import React, { useContext, useState } from "react";
 
-import { ExchangeCoinContext } from "../../../exchangeCoinContext";
 import { CollectionMockContext } from "../../../collectionMock";
 import { CartItemsContext } from "../../../cartItemsContext";
-import { Icons } from "../../../constants";
+import { ItemsListMapping } from "./ItemsListmap";
 
 export const MockItemsList = () => {
-  const currentCoin = useContext(ExchangeCoinContext);
   const [mockList] = useContext(CollectionMockContext);
   const [cartItems, setCartItems] = useContext(CartItemsContext);
   const [colorSelect, setColorSelect] = useState("White");
@@ -33,6 +31,7 @@ export const MockItemsList = () => {
       },
     ]);
   };
+  console.log(cartItems.length);
 
   const onEventHandler = (data) => {
     setColorSelect(data);
@@ -41,73 +40,19 @@ export const MockItemsList = () => {
 
   return (
     <div className="flex grid grid-cols-2 gap-2 lg:grid lg:grid-cols-3 gap-8 xl:grid-cols-4 gap-16 p-20 ">
-      <h1>{colorSelect}</h1>
       {mockList.map(
-        ({ name, price, quantity, colors, sizes, id, image, category }) => (
-          <div key={id} className="flex  justify-evenly max-w-[350px]">
-            <div className="rounded-xl font-black p-4 border-2 shadow-[0_15px_60px_15px_rgba(0,0,0,0.1)]">
-              <div className="flex relative flex-col">
-                <img
-                  className="rounded-xl lg:max-w-[200px]"
-                  src={image}
-                  alt={category}
-                />
-                <div className="flex flex-col flex ">
-                  <span className="text-center text-xl pb-2">{name}</span>
-                  {currentCoin[0].value === "Dolar - $" ? (
-                    <span>Price: {price}</span>
-                  ) : (
-                    <span>Price: {(price * 5.1).toFixed(2)}</span>
-                  )}
-                  <span>Stock: {quantity}</span>
-                  <select
-                    className=" text-black rounded-xl mb-2 mt-2"
-                    onChange={(e) => onEventHandler(e.target.value)}
-                    value={colorSelect}
-                  >
-                    {colors.map((option) => (
-                      <option
-                        className="w-20"
-                        key={option}
-                        value={option}
-                        label={option}
-                      />
-                    ))}
-                  </select>
-                  <select className="rounded-xl">
-                    {sizes.map((option) => (
-                      <option key={option} value={option} label={option} />
-                    ))}
-                  </select>
-                </div>
-                <div className="relative pt-12 hover:opacity-80 ">
-                  <div className="absolute bottom-0 right-2 flex border-solid border-2 border-sky-500  rounded-full p-1 float-right cursor-pointer">
-                    <Icons.CartIcon
-                      width="20px"
-                      height="20px"
-                      className=""
-                      fill="#3FBCF4"
-                    />
-                    <input
-                      type="button"
-                      value="Buy"
-                      className="pl-1 pr-1 text-black"
-                      onClick={() =>
-                        buyButtonHandler(
-                          name,
-                          price,
-                          colors,
-                          sizes,
-                          id,
-                          category,
-                        )
-                      }
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        ({ name, price, quantity, colors, sizes, image, category }) => (
+          <ItemsListMapping
+            name={name}
+            price={price}
+            quantity={quantity}
+            colors={colors}
+            sizes={sizes}
+            image={image}
+            category={category}
+            onEventHandler={onEventHandler}
+            buyButtonHandler={buyButtonHandler}
+          />
         ),
       )}
     </div>
