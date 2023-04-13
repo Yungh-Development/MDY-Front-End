12c4130322331693deff2e9a18b8548987a70daa
GabrielOliveira00 <72420671+GabrielOliveira00@users.noreply.github.com>
2023-04-12 23:07:23 -0300

    Delete shopping-cart/src/shoppingCartProject directory

--- shopping-cart/src/shoppingCartProject/arrayToOptions.jsx
+++
@@ -1,7 +0,0 @@
-const arrayToOptions = (items) =>
-  items.map((item) => ({
-    label: item,
-    value: item,
-  }));
-
-export default arrayToOptions;
--- shopping-cart/src/shoppingCartProject/getUniqueKeyFromItem.jsx
+++
@@ -1,3 +0,0 @@
-const getUniqueKeyFromItem = (item) => `${item.id}-${item.size}-${item.color}`;
-
-export default getUniqueKeyFromItem;
