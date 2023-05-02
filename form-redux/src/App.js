38335fb5a983239e89178cd6d412a888aad7f805
Gabriel <mgabriel.moliveira@gmail.com>
2023-05-02 18:08:22 -0300

    Redone validations and restyling

--- form-redux/src/App.js
+++ form-redux/src/App.js
@@ -4,8 +4,31 @@ import { SignInForm } from "./Components/SignInForm";
 import "./App.css";
 
 function App() {
-  const SignInHandler = (data) => {
-    console.log(data);
+  const SignInHandler = (val) => {
+    const errors = {};
+    if (!val.firstName) {
+      alert("First Name is required");
+      errors.firstName = "Required";
+    }
+    if (!val.lastName) {
+      alert("Last Name is required");
+      errors.lastName = "Required";
+    }
+    if (!val.email) {
+      alert("email is required");
+      errors.email = "Required";
+    } else if (!/^.+@.+$/i.test(val.email)) {
+      alert("email is invalid");
+      errors.email = "Invalid email address";
+    }
+    if (!val.age) {
+      errors.age = "Required";
+    } else if (isNaN(Number(val.age))) {
+      errors.age = "Must be a number";
+    } else if (Number(val.age) < 18) {
+      errors.age = "Sorry, you must be at least 18 years old";
+    }
+    return errors;
   };
   return (
     <div className="App">
